46d488a8c56d96cf80bdb3385a452bff3d5a6dfb
falak50 <83400991+falak50@users.noreply.github.com>
2023-12-31 11:12:27 -1200

    profile section complite\

--- src/pages/Profile/Profile/Profile.jsx
+++ src/pages/Profile/Profile/Profile.jsx
@@ -1,12 +1,13 @@
 import { useContext } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { AuthContext } from '../../../providers/AuthProviders';
-// import { MdOutlineModeEditOutline } from "react-icons/md";
+import { MdOutlineModeEditOutline } from "react-icons/md";
 import bkimg from '../../../assets/backgroudPIC.jpg'
 import profileimg from '../../../assets/profilePic.jpg'
 
 const Profile = () => {
-    const {user} = useContext(AuthContext)
+    const {user} = useContext(AuthContext);
+    
     return (
         <div>
             <Helmet>
@@ -19,19 +20,63 @@ const Profile = () => {
 
                         <div className=''>
                         <img className='h-[200px] w-full rounded-t-lg  object-cover' src={bkimg} alt="" />
+                        
                         </div>
                                    
-
+                         
                     <div className='mt-[-100px]'>
-                    <div className="avatar mx-4  ">
+                    <div className="avatar mx-4  flex">
                     <div className="w-40 rounded-full ring ring-[white] ring-offset-base-100 ring-offset-2">
                         <img src={profileimg} />
                     </div>
+                    <button className="btn btn-circle   bg-[#ededec] text-[#6a6a6a] text-2xl ml-auto mt-[-90px]">
+                    <MdOutlineModeEditOutline className=''/>
+                     </button>
                     </div>
-                    <div className='mx-4'>
-                    <h1 className='text-2xl '>Falak Ahmed Shakib</h1>
-                    </div>    
+                    <div className='mx-4 flex'>
+                    <div>
+                    <h1 className='text-3xl font-[600]'>Falak Ahmed Shakib</h1>
+                    <p>MERN Stack Web Developer || Competitive Programmer</p>
+                     <div className='flex'>
+                     <span>Sylhet, Bangladesh .</span> 
+                     <div className='mx-4 font-[600] text-[#2779c9]'>
+                     <a href="" > Contact info</a>
+                     </div>
+                    
+                     </div>
+                     <div className='font-[600] text-[#2779c9]'>
+                     <a href="" >500+ connections</a>
+                     </div>
+                     <div className='mt-3 mx-0'>
                         
+                     <button className="btn btn-sm  mx-0 rounded-[50px] bg-[#0a66c2] hover:bg-[#004182] text-[white]">Open to</button>
+                     <button className="btn btn-sm mx-2 rounded-[50px] btn-outline hover:bg-opacity-20 hover:bg-[#0a66c2] text-[#0a66c2] hover:text-[#0a66c2] btn-ghost">Add profile section</button>
+                     <button className="btn btn-sm mx-2 rounded-[50px] btn-outline hover:bg-opacity-20 hover:bg-[#767676]  text-[#767676] hover:text-[#767676] btn-ghost">More</button>
+                     </div>
+
+                       
+
+                    </div>
+                    
+                    <button className="btn btn-circle  bg-[#ededec] text-[#6a6a6a] text-2xl ml-auto">
+                    <MdOutlineModeEditOutline className=''/>
+                     </button>
+
+                    </div>    
+                    <div className='flex p-2 mx-4 w-[70%] m-4  bg-opacity-20 bg-[#0a66c2]  rounded-xl  justify-between'>
+                           <div className='mx-2'>
+                            <h1>Open to work</h1>
+                            <p>Software Engineer, React Developer, Javascript Developer and Programmer role</p>
+                            <div className='font-[600] text-[#2779c9]'>
+                     <a href="" >Show details</a>
+                     </div>
+                           </div>
+                           <div className='m-1'>
+                           <button className="btn btn-circle  bg-[#ededec] text-[#6a6a6a] text-2xl ">
+                            <MdOutlineModeEditOutline className=''/>
+                            </button>
+                           </div>
+                       </div>
                     </div> 
 
                     
@@ -46,9 +91,7 @@ const Profile = () => {
                  <h1>Profile languagee</h1>
                  <span>www.linkedin.com/in/falak-ahmed-shakib-a5182823a</span>
                 </div>
-                
-             
-                
+   
             </div>
                 
             </div>
@@ -59,3 +102,5 @@ const Profile = () => {
 };
 
 export default Profile;
+
+
