3da1a2a6e3349058dac02da6ab8b4ea1f8ae287b
falak50 <83400991+falak50@users.noreply.github.com>
2024-10-30 18:28:08 +0600

    update give post

--- src/components/HomeCompo/MypostHome.jsx
+++ src/components/HomeCompo/MypostHome.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext, useRef, useState, useEffect } from "react";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
@@ -8,25 +8,20 @@ import { IoMdClose } from "react-icons/io";
 import { IoImagesSharp } from "react-icons/io5";
 import axios from "axios";
 import useMypost from "../../hooks/useMypost";
-import {
-  CardActionArea,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  IconButton,
-} from "@mui/material";
-import { Avatar, Card, Typography } from "antd";
-import dpImg from "../../assets/dpImg.jpg";
+import { IconButton, Button } from "@mui/material";
 import { AuthContext } from "../../providers/AuthProviders";
+import { Image } from "antd"; // Assuming you're using Ant Design for Image
+
 const pathLink = "http://localhost:5000/images/";
+
 const MypostHome = ({ open, setOpen, setResetCount }) => {
-  // const [userInfo, ] = useUserinfo();
+  const [showCard, setShowCard] = useState(true);
   const { curUser } = useContext(AuthContext);
   const owner = JSON.parse(localStorage.getItem("user"));
-  const [, Mypostsrefetch, ,] = useMypost();
+  const [, Mypostsrefetch] = useMypost();
   const [files, setFiles] = useState([]);
   const fileInputRef = useRef(null);
-  const [showCard,setShowCard] = useState(true)
+
   const handleAddImage = () => {
     if (fileInputRef.current) {
       fileInputRef.current.click();
@@ -37,11 +32,12 @@ const MypostHome = ({ open, setOpen, setResetCount }) => {
     const fileList = Array.from(e.target.files);
     setFiles((prevFiles) => [...prevFiles, ...fileList]);
   };
+
   const handleRemoveImage = (index) => {
     setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
   };
 
-  const formRef = React.useRef(null);
+  const formRef = useRef(null);
   const handleClose = () => {
     setOpen(false);
   };
@@ -51,14 +47,12 @@ const MypostHome = ({ open, setOpen, setResetCount }) => {
       description: "",
     },
   });
+
   const onSubmit = (data) => {
-    console.log("data---->", data);
     const uid = owner?._id;
     const formData = new FormData();
 
     for (let i = 0; i < files.length; i++) {
-      files[i].title = "image";
-      console.log("file info ", files[i]);
       formData.append(`file`, files[i]);
     }
 
@@ -68,27 +62,77 @@ const MypostHome = ({ open, setOpen, setResetCount }) => {
     axios
       .post("http://localhost:5000/posts", formData)
       .then((res) => {
-        console.log("hello ", res);
         Mypostsrefetch();
       })
       .catch((err) => console.log(err));
 
-    console.log("come my opost");
     setOpen(false);
     reset();
     setResetCount((p) => p + 1);
   };
 
-  const handleExternalSubmit = () => {
-    if (formRef.current) {
-      handleSubmit(onSubmit)();
-    }
+  const renderMedia = (file) => {
+    const mediaType = file.type.split("/").pop().toLowerCase(); // Get the file type from the file object
+    const mediaSrc = URL.createObjectURL(file); // Create a local URL for the file
+
+    // List of accepted image file extensions
+    const imageExtensions = ["jpg", "jpeg", "png", "gif", "bmp", "svg", "webp"];
+
+    return (
+      <div
+        style={{
+          display: "block",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          marginBottom: "10px",
+        }}
+      >
+        {(() => {
+          switch (mediaType) {
+            case "mp4":
+              return (
+                <video controls className="w-full h-full">
+                  <source src={mediaSrc} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              );
+            default:
+              if (imageExtensions.includes(mediaType)) {
+                return (
+                  <Image
+                    width={300}
+                    src={mediaSrc} // Use the mediaSrc created above
+                    alt={`Post image`}
+                    className="object-cover w-full h-full"
+                  />
+                );
+              } else {
+                return (
+                  <div>
+                    <a
+                      href={mediaSrc}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button type="link">File: {file.name} </Button>{" "}
+                      {/* Button for unknown file types */}
+                    </a>
+                  </div>
+                );
+              }
+          }
+        })()}
+      </div>
+    );
   };
 
-  const handleInput = (event) => {
-    event.target.style.height = "auto";
-    event.target.style.height = event.target.scrollHeight + "px";
-  };
+  useEffect(() => {
+    return () => {
+      // Cleanup URLs to prevent memory leaks
+      files.forEach((file) => URL.revokeObjectURL(URL.createObjectURL(file)));
+    };
+  }, [files]);
   const curdate = new Date().toLocaleDateString("en-US", {
     year: "numeric",
     month: "long",
@@ -127,6 +171,8 @@ const MypostHome = ({ open, setOpen, setResetCount }) => {
         }}
       >
         <DialogTitle id="scroll-dialog-title" className="flex justify-between">
+          {/* <div className="flex items-center mb-2"> */}
+
           <div className="flex items-center mb-2">
             <div className="avatar">
               <div className="w-12 rounded-full">
@@ -143,8 +189,8 @@ const MypostHome = ({ open, setOpen, setResetCount }) => {
               </div>
               <div className="text-gray-600 text-sm">{curdate}</div>
             </div>
+            {/* </div> */}
           </div>
-
           <button
             onClick={handleClose}
             className="btn btn-circle border-none bg-white hover:bg-[#ededec] text-[#6a6a6a] text-2xl "
@@ -152,17 +198,18 @@ const MypostHome = ({ open, setOpen, setResetCount }) => {
             <IoMdClose />
           </button>
         </DialogTitle>
-        <DialogContent dividers={scroll}>
+
+        <DialogContent dividers>
           <form
             ref={formRef}
             onSubmit={handleSubmit(onSubmit)}
-            className="card-body mb-0 mt-0 top-[-20px] relative"
+            className="mb-0"
           >
-            <div className="form-control">
+            <div className="form-control p-auto ">
               <textarea
-                onInput={handleInput}
                 placeholder="What do you want to talk about?"
                 {...register("description")}
+                className="w-full"
                 style={{
                   overflowY: "hidden",
                   width: "100%",
@@ -173,46 +220,39 @@ const MypostHome = ({ open, setOpen, setResetCount }) => {
                 }}
               />
             </div>
-        
-            {(showCard && files.length === 0) && (
-  <div > 
+            {showCard && files.length === 0 && (
+              <div>
+                <div className="flex justify-between items-center relative p-4 pd m-2">
+                  <h1></h1>
+                  <IconButton
+                    onClick={() => setShowCard(false)}
+                    className="text-gray-600 hover:text-gray-900"
+                  >
+                    <IoMdClose />
+                  </IconButton>
+                </div>
 
-    <div  className="flex justify-between items-center relative p-4 m-2">
-      <h1 ></h1> 
-      <IconButton
-        onClick={() => setShowCard(false)}
-        className="text-gray-600 hover:text-gray-900"
-      >
-        <IoMdClose />
-      </IconButton>
-    </div>
-
-    <div onClick={handleAddImage} className="flex-grow flex justify-center items-center"> 
-  <div className="flex flex-col items-center">
-    <p className="font-serif text-center">Add your Photos</p> {/* Center text horizontally */}
-    <IoImagesSharp className="text-[#43b661] text-4xl" />
-  </div>
-</div>
-  </div>
-)}
-
-
-
-
-
-
-
+                <div
+                  onClick={handleAddImage}
+                  className="flex-grow flex justify-center items-center pd-10"
+                >
+                  <div className="flex flex-col items-center">
+                    <p className="font-serif text-center">Add your file</p>{" "}
+                    {/* Center text horizontally */}
+                    <IoImagesSharp className="text-[#43b661] text-4xl" />
+                  </div>
+                </div>
+                <h1></h1>
+                <h1></h1>
+              </div>
+            )}
             <div>
               {files.map((file, index) => (
                 <div
                   key={index}
-                  style={{ position: "relative", display: "inline-block" }}
+                  style={{ position: "relative", display: "block" }}
                 >
-                  <img
-                    src={URL.createObjectURL(file)}
-                    alt={`Image ${index}`}
-                    style={{ marginRight: "10px" }}
-                  />
+                  {renderMedia(file)} {/* Use renderMedia for uploaded files */}
                   <button
                     type="button"
                     className="btn btn-circle btn-sm"
@@ -227,17 +267,9 @@ const MypostHome = ({ open, setOpen, setResetCount }) => {
           </form>
         </DialogContent>
 
-        <DialogActions
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            padding: "8px 24px", // Optional: Add some padding for better spacing
-          }}
-        >
+        <DialogActions>
           <button className="btn btn-square btn-md" onClick={handleAddImage}>
-            <IconButton>
             <IoImagesSharp className="text-[#43b661] " />
-            </IconButton>
           </button>
           <input
             ref={fileInputRef}
@@ -247,8 +279,9 @@ const MypostHome = ({ open, setOpen, setResetCount }) => {
             style={{ display: "none" }}
           />
           <button
-            onClick={handleExternalSubmit}
-            className="btn bg-[#0a66c2] text-white rounded-full  px-6 py-2 text-xl mr-2"
+            type="submit"
+            onClick={handleSubmit(onSubmit)}
+            className="btn bg-[#0a66c2] text-white rounded-full px-6 py-2 text-xl mr-2"
           >
             Post
           </button>
