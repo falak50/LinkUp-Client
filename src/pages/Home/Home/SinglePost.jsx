d102367694bd1f6d9bcc6670086d1ff6afe7a61e
falak50 <83400991+falak50@users.noreply.github.com>
2024-11-04 10:31:22 +0600

    update singlePost

--- src/pages/Home/Home/SinglePost.jsx
+++ src/pages/Home/Home/SinglePost.jsx
@@ -1,46 +1,57 @@
 import { Spin } from 'antd';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Post from './Post';
 
 export default function SinglePost() {
     const { id } = useParams();
-    const [post, setPost] = useState({});
+    const [post, setPost] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState('');
+
     useEffect(() => {
         setIsLoading(true);
         axios
-          .get(`http://localhost:5000/posts/post/${id}`)
-          .then((response) => {
-            const fetchedProduct = response.data[0];
-            setPost(fetchedProduct);
+            .get(`http://localhost:5000/posts/post/${id}`)
+            .then((response) => {
+                const fetchedProduct = response.data[0];
+                if (fetchedProduct) {
+                    setPost(fetchedProduct);
+                    setErrorMessage('');
+                } else {
+                    setErrorMessage('This post was deleted or does not exist.');
+                }
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching product:', error);
+                setErrorMessage('An error occurred while fetching the post.');
+                setIsLoading(false);
+            });
+    }, [id]);
 
-            setIsLoading(false);
-           
-          })
-          .catch((error) => {
-            console.error('Error fetching product:', error);
-            setIsLoading(false);
-          });
-      }, []);
-
-      if (isLoading) {
+    if (isLoading) {
         return (
-          <div className="flex justify-center items-center h-screen">
-            <Spin size="large" />
-          </div>
+            <div className="flex justify-center items-center h-screen">
+                <Spin size="large" />
+            </div>
         );
-      }
-      console.log('post',post)
+    }
 
-  return (
-    <div className="flex justify-center  min-h-screen">
-    <div className="w-full lg:w-[55%]">
-      {/* <p>{id}</p> */}
-      {/* <post post={post}></post> */}
-      <Post post={post}></Post>
-    </div>
-    </div>
-  )
+    if (errorMessage) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <p>{errorMessage}</p>
+            </div>
+        );
+    }
+
+    return (
+        <div className="flex justify-center min-h-screen">
+            <div className="w-full lg:w-[55%]">
+                <Post post={post} />
+            </div>
+        </div>
+    );
 }
--- src/pages/Profile/Profile/Profile.jsx
+++ src/pages/Profile/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import Skills from '../../../components/ProfileElement/Skills/Skills';
 import Awards from '../../../components/ProfileElement/Awards/Awards';
 import Mypost from '../../../components/ProfileElement/Mypost/Mypost';
 import { Link, useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import useUserinfo from '../../../hooks/useUserinfo';
 import Swal from 'sweetalert2';
 import Check from '../../../not_includes/Check';
@@ -16,15 +16,17 @@ import MessagingPage from '../../Messaging/MessagingPage';
 import Dot from '../../../not_includes/Dot';
 import PostEdit from '../../../components/ProfileElement/Mypost/PostEidt';
 import Load from '../../../components/Load';
+import { AuthContext } from '../../../providers/AuthProviders';
 
 const Profile = () => {
   const [owner, setOwner] = useState(JSON.parse(localStorage.getItem('user')));
   const { email } = useParams(); 
-
+  const {  curUser } = useContext(AuthContext);
   const [relation, setRelation] = useState("");
   const [redender, setRedender] = useState(0);
   const [loading, setLoading] = useState(true);
   const [userInfo] = useUserinfo();
+  console.log('user info ',userInfo)
   const [open, setOpen] = useState(false);
   useEffect(() => {
     if (userInfo?.email && owner?.email) {
@@ -80,12 +82,13 @@ const Profile = () => {
         <div className="md:flex md:flex-col m-2 gap-4 w-[75%]">
           <div>
             <Intro owner={owner} email={email} relation={relation} setRelation={setRelation} setRedender={setRedender} />
+            
             <Education />
             <Skills />
             <Mypost className='' />
             <br />
-            <Awards className='mt-4' />
-            <br />
+            {/* <Awards className='mt-4' />
+            <br /> */}
             <br />
           </div>
         </div>
